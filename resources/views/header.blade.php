fe20080cc999b1edc74af9dccac0087f8756f491
Stanislav Chrobak <vtt.chrobak@gmail.com>
2023-03-17 16:26:49 +0100

    updated header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,1 +1,13 @@
-<h1>Header</h1>
+<header>
+    <div class="container bg-light px-0">
+        <!-- Top navbar --> 
+        <nav class="nav justify-content-end">
+            <a class="nav-link disabled" href="#">Prihlásiť</a>
+            <a class="nav-link active" href="#">Registrovať</a>
+        </nav>
+
+        <!-- Banner-->
+        <div style="background-image: url('../img/ZM.jpg'); width: 100%;  height: 363px; max-height: 363px;">
+        </div>
+    </div>
+</header>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,18 +11,14 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         -->
         <!-- Styles -->
-      
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
 
     <!-- Header -->            
     @include('header')
-    -
-    *
-    *
-    *
-    *
-    *
-    *
-    
+    <!-- Content -->  
+    <br>
+    <br>
+    <!-- Footer -->  
     @include('footer')
 </html>
