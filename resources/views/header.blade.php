ce5d18b62d173c940b004df239c1031f6798c8bf
Stanislav Chrobak <vtt.chrobak@gmail.com>
2023-03-18 16:49:12 +0100

    Header template complete

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="container bg-light px-0">
+    <div class="container bg-light px-1">
         <!-- Top navbar --> 
         <nav class="nav justify-content-end">
             <a class="nav-link disabled" href="#">Prihlásiť</a>
@@ -7,7 +7,34 @@
         </nav>
 
         <!-- Banner-->
-        <div style="background-image: url('../img/ZM.jpg'); width: 100%;  height: 363px; max-height: 363px;">
+        <div class="d-flex align-items-center" style="background-image: url({{url('images/ZM.jpg')}}); width: 100%;  height: 363px; height: 363px;">
+       
+            <div class="card mb-0 mx-1" style="max-width: 540px; opacity: 90%;">
+                <div class="row g-0">
+                    <div class="col-sm-2 text-center d-flex align-items-center ">
+                        <img src="{{url('images/matica_logo.jpg')}}" class="img-fluid"  style="max-height: 150px;" alt="matica_logo">
+                    </div>
+                    <div class="col-sm-10">
+                        <div class="card-body">
+                            <h5 class="card-title">Miestny odbor Matice slovenskej<br>Zlaté Moravce</h5>
+                            <p class="card-text">Národno-duchovný pilier štátu ako jediná masovo udržiava a reálne rozvíja Matica slovenská.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+
+        <!-- Main navbar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" aria-current="page" href="#">Úvod</a>
+                    <a class="nav-item nav-link" href="#">Galéria</a>
+                    <a class="nav-item nav-link" href="#">Kontakt</a>
+                </div>
+            </div>
+        </nav>
 </header>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>MO MS ZM</title>
 
-        <!-- Fonts 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        -->
         <!-- Styles -->
+        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        
+        
     </head>
-
+    <body>
     <!-- Header -->            
     @include('header')
     <!-- Content -->  
-    <br>
-    <br>
+    
+    
     <!-- Footer -->  
     @include('footer')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
 </html>
